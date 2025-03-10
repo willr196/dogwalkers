1c87bb6d2929e37daadca58f78f39868859ecc45
willr196 <willrobb11@gmail.com>
2025-03-10 11:44:27 +0000

    adding new files

---
+++ dogwalking/resources/views/website/main_master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Dog Walking Website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+
+
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/jquery.timepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+  </head>
+  <body>
+  @include('website.body.header');
+  <main>
+  @yield('main')
+  </main>
+
+
+
+  @include('website.body.footer');
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('assets/js/popper.min.js')}}"></script>
+  <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.easing.1.3.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.waypoints.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.stellar.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.animateNumber.min.js')}}"></script>
+  <script src="{{asset('assets/js/bootstrap-datepicker.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.timepicker.min.js')}}"></script>
+  <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{asset('assets/js/scrollax.min.js')}}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{asset('assets/js/google-map.js')}}"></script>
+  <script src="{{asset('assets/js/main.js')}}"></script>
+  
+
+</body>
